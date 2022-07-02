f974bed389a46c8470869db69502414af3363025
piyush-1871 <piyush1871b@gmail.com>
2022-07-02 22:45:14 +0530

    make a calculator using HTML CSS and JS

---
+++ calc.js
@@ -0,0 +1,21 @@
+let screen = document.getElementById('screen');
+let buttons = document.querySelectorAll('button');
+let screenValue = '';
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText = e.target.innerText;
+        console.log('Button text is',buttonText);
+        if(buttonText=='C'){
+            screenValue = "";
+            screen.value = screenValue;
+        }else if(buttonText=='*'){
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }else if(buttonText=='='){
+            screen.value = eval(screenValue);
+        }else{
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    });
+}
